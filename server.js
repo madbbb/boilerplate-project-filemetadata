a9fb122f77e34d8dcd79e20494b0b76de1b941d6
Bogdan Korenkov <madbbb@bogdans-macbook-pro.local>
2019-03-24 10:12:23 +0200

    initial

--- server.js
+++ server.js
@@ -4,20 +4,31 @@ var express = require('express');
 var cors = require('cors');
 
 // require and use "multer"...
+var multer = require('multer');
+var storage = multer.memoryStorage();
+var upload = multer({ storage });
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.listen(process.env.PORT || 3000, function () {
+app.get('/hello', function(req, res) {
+  res.json({ greetings: 'Hello, API' });
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
+
+app.listen(process.env.PORT || 3000, function() {
   console.log('Node.js listening ...');
 });
